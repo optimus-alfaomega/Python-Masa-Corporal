8063e0fa507d7c846e8a9030a4cd601534b04abc
Optimus-alfaomega <a-and-o-technologies@outlook.com>
2022-04-22 15:44:53 -0500

    first commit

---
+++ IndiceMasaCorporal.py
@@ -0,0 +1,18 @@
+#conversion de pies a metros y centimetros para los que usan sistema ingles 
+def piespulgam(pies, pulgadas = 0.0):
+    return pies * 0.3048 + pulgadas * 0.0254
+
+#conversion de libras a Kilos
+def lbsakg(lb):
+    return lb * 0.45359237
+
+
+def imc(peso, altura):
+    if altura < 1.0 or altura > 2.5 or \
+    peso < 20 or peso > 200:
+        return None
+    
+    return peso / altura ** 2
+
+
+print(imc(peso = lbsakg(176), altura = piespulgam(5, 7)))
